22cd8a4a1f8090c96af55fae755aad7d70f1b1c3
Aser <aseralemu03@gmail.com>
2026-02-07 01:59:27 +0300

    codeforces solution: sum

---
+++ week3/codeforces/A_Sum.py
@@ -0,0 +1,12 @@
+def solve():
+    n = int(input())
+
+    for i in range(n):
+        a, b, c = map(int, input().split())
+
+        if a == b + c or b == a + c or c == a + b:
+            print("YES")
+        else:
+            print("NO")
+
+solve()
