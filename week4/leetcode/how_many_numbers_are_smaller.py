f6e029b8370cfde699cbf08984f9a218a7136463
Aser <aseralemu03@gmail.com>
2026-02-12 09:39:40 +0300

    leetcode solution: how many numbers are smaller than current number

--- week4/leetcode/how_many_numbers_are_smaller.py
+++ week4/leetcode/how_many_numbers_are_smaller.py
@@ -4,12 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[j] < nums[i]:
-                    count += 1
-            result.append(count)
+        sorted_nums = sorted(nums)
+        smaller_count = {}
+
+        for i in range(len(sorted_nums)):
+            if sorted_nums[i] not in smaller_count:
+                smaller_count[sorted_nums[i]] = i
         
-        return result
+        return [smaller_count[num] for num in nums]
