9edde1ceffd876cd613a3a374e60cd07a21ee46c
Aser <aseralemu03@gmail.com>
2026-02-14 23:02:29 +0300

    leetcode solution: count equal and divisible pairs

---
+++ week4/leetcode/count_equal_and_divisible_pairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] == nums[j]:
+                    if i != j and (i * j) % k == 0:
+                        count += 1
+        
+        return count / 2
+        
