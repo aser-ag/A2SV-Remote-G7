4f35b1519ef60641a31e8fc7a810d5f59a1df4f9
Aser <aseralemu03@gmail.com>
2026-03-03 21:34:01 +0300

    Add solution for Move Zeroes (trial 4)

---
+++ leetcode/move-zeroes_trial4.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        holder = 0
+
+        for seeker in range(len(nums)):
+            if nums[seeker] != 0:
+                nums[seeker], nums[holder] = nums[holder], nums[seeker]
+                holder += 1
