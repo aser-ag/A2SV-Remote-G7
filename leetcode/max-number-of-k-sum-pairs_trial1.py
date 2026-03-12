f95005c07b328065e84d336700f166e099ad51d6
Aser <aseralemu03@gmail.com>
2026-03-12 13:43:33 +0300

    Add solution for Max Number of K-Sum Pairs (trial 1)

---
+++ leetcode/max-number-of-k-sum-pairs_trial1.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort()
+
+        left, right = 0, len(nums) - 1
+        count = 0
+
+        while left < right:
+            if nums[left] + nums[right] == k:
+                count += 1
+                left += 1
+                right -= 1
+            
+            elif nums[left] + nums[right] < k:
+                left += 1
+            
+            else:
+                right -= 1
+        
+        return count
