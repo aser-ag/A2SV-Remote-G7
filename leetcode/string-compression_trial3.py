48aaabc4077cfdaaf22a651a5f3d173729e4d12e
Aser <aseralemu03@gmail.com>
2026-03-15 16:24:51 +0300

    Add solution for String Compression (trial 3)

---
+++ leetcode/string-compression_trial3.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        left, right = 0, 0
+        n = len(chars)
+        res = []
+        stop = 0
+        while right < n:
+            count = 0
+            while right < n and chars[left] == chars[right]:
+                right += 1
+                count += 1
+
+            if count == 1:
+                chars[stop] = chars[left]
+                stop += 1
+            else:
+                chars[stop] = chars[left]
+                stop += 1
+                for num in str(count):
+                    chars[stop] = num
+                    stop += 1
+            left = right
+
+        return stop
