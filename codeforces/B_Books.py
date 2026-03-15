be227fd67791b9cbe0abef839a9df910e25337d2
Aser <aseralemu03@gmail.com>
2026-03-15 23:43:53 +0300

    codeforces solution: books

---
+++ codeforces/B_Books.py
@@ -0,0 +1,17 @@
+n, t = map(int, input().split())
+books = list(map(int, input().split()))
+
+left = 0
+current_sum = 0
+best = 0
+
+for right in range(n):
+    current_sum += books[right]
+
+    while current_sum > t:
+        current_sum -= books[left]
+        left += 1
+
+    best = max(best, right - left + 1)
+
+print(best)
